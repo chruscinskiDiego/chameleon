72239b1b1626d38316a7ee5dde2fa37c48ba7fed
chruscinskiDiego <121837842+chruscinskiDiego@users.noreply.github.com>
2024-02-24 22:47:58 -0300

    Editor Config for the code

--- src/server/Server.ts
+++ src/server/Server.ts
@@ -3,7 +3,8 @@ import express from 'express';
 const server = express();
 
 server.get('/', (req, res) => {
-    return res.send('Sucess GET');
+  return res.send('Sucess GET');
 });
 
-export {server};
+
+export { server };
